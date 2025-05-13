da3abf6b58980a5cb659b49152774847be6bffcf
Etienne <etienne.a2004@gmail.com>
2025-05-13 10:15:18 +0200

    ajout du tableau patchPosition qui garde la position des patchs meme quand ils sont en vecteur

--- src/image/ImageDebruitee.java
+++ src/image/ImageDebruitee.java
@@ -1,6 +1,8 @@
 package image;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 import javafx.scene.image.Image;
 
@@ -25,14 +27,56 @@ public class ImageDebruitee {
 		Integer taillePatch = 10;
 		Integer ligne = 100;
 		Integer colonne = 100;
-		List<Patch> patchs = ExtractPacths(imageBruitee, taillePatch);
+		Integer nbLignePatch = 10;
+		Integer nbColonnePatch = 10;
 		
+		List<Patch> patchs = extractPacths(imageBruitee, taillePatch);
 		
+		List<int[]> patchPosition = getPositions(patchs);
 		
-		imageDebruitee = ReconstructPatchs()
+		vecteurs = vectorPatchs(patchs);
 				
-		return imageDebruitee
+		// transformation des vecteurs
+		//...
 		
+		
+		Vector<Float> vecteursDebruitee;
+		
+		patchsDebruitee = patchsVector(vecteursDebruitee, nbLignePatch, nbColonnePatch, patchPosition);
+				
+		imageDebruitee = reconstructPatchs(patchsDebruitee);
+				
+		return imageDebruitee;
+		
+	}
+	
+	// Etienne Angé
+	public List<int[]> getPositions(List<Patch> patchs){
+		List<int[]> patchPosition = new ArrayList<>();
+		
+		for(Patch patch : patchs) {
+			int[] pos = new int[2];
+			pos[0] = patch.getX();
+			pos[1] = patch.getY();
+			patchPosition.add(pos);
+		}
+		
+		return patchPosition;
+	}
+	
+	// Etienne Angé
+	public List<Patch> patchsVector(List<Vector<Float>> vecteurs,Integer nbLigne, Integer nbColonne, List<int[]> patchPosition){
+		
+		
+		List<Patch> patchs = new ArrayList<>();
+		int i = 0;
+		for(Vector<Float> vecteur : vecteurs) { // pour chaque vecteur
+			
+			patchs.add(new Patch(vecteur, nbColonne, nbLigne, patchPosition.get(i)[0], patchPosition.get(i)[1]) );
+			
+		}
+		
+		return patchs;
 	}
 
 }
