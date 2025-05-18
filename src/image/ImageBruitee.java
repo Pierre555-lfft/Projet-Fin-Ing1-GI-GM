7e7f59fd85901fe22b4529995a73f998fbd3a5de
Mathuw6433 <md10022004@gmail.com>
2025-05-18 12:08:46 +0200

    Update ImageBruitee.java

--- src/image/ImageBruitee.java
+++ src/image/ImageBruitee.java
@@ -11,6 +11,34 @@ public class ImageBruitee {
     public ImageBruitee(Image image, double ecartType) {
         this.imageBruitee = ajouterBruit(image, ecartType);
     }
+    public ImageBruitee(Image image, double ecartType, String wb) {
+    	this.imageBruitee = ajouterBruit_wb(image, ecartType);
+    }
+
+        private Image ajouterBruit_wb(Image original, double ecartType) {
+        int width = (int) original.getWidth();
+        int height = (int) original.getHeight();
+
+        WritableImage result = new WritableImage(width, height);
+        PixelReader reader = original.getPixelReader();
+        PixelWriter writer = result.getPixelWriter();
+
+        Random rand = new Random();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Color color = reader.getColor(x, y);
+                
+                double gris = 0.299 * color.getRed() + 0.587 * color.getGreen() + 0.114 * color.getBlue();
+                gris = clamp(gris + rand.nextGaussian()* ecartType / 255);
+
+
+                writer.setColor(x, y, new Color(gris, gris, gris, color.getOpacity()));
+            }
+        }
+
+        return result;
+    }
 
     private Image ajouterBruit(Image original, double ecartType) {
         int width = (int) original.getWidth();
